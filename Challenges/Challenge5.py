fa5a2b18a9f770301bb86f6cc7ba584a5924e352
Rohith G Krishna <rohithgk2@gmail.com>
2021-02-17 17:12:11 +0530

    Challenge5Commit

---
+++ Challenges/Challenge5.py
@@ -0,0 +1,22 @@
+
+def listItems(itemList,value):
+    """This is not my idea."""
+    indexList = []
+    for i in range(len(itemList)):
+        if itemList[i] == value:
+            indexList.append([i])
+            # print(indexList)
+        elif isinstance(itemList[i],list):
+            for index in listItems(itemList[i],value):
+                print(index)
+                indexList.append([i]+index)
+    return indexList
+
+def main():
+    itemList = [[1,2],[2,3]]
+    # itemList = [[[1,2,3],2,[1,3]],[1,2,3]]
+    # value  = int(input("Enter the value to be searched"))
+    print(listItems(itemList,2))
+
+if __name__ == '__main__':
+    main()
