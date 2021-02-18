10d3720967ab3a46b35e19ee6188fad72749bdcd
Rohith G Krishna <rohithgk2@gmail.com>
2021-02-18 10:44:24 +0530

    Commit

--- Challenges/Challenge5.py
+++ Challenges/Challenge5.py
@@ -8,14 +8,13 @@ def listItems(itemList,value):
             # print(indexList)
         elif isinstance(itemList[i],list):
             for index in listItems(itemList[i],value):
-                print(index)
-                indexList.append([i]+index)
+                indexList.append([i] + index)
     return indexList
 
 def main():
-    itemList = [[1,2],[2,3]]
-    # itemList = [[[1,2,3],2,[1,3]],[1,2,3]]
-    # value  = int(input("Enter the value to be searched"))
+    # itemList = [[1,2],[2,3]]
+    itemList = [[[1,2,3],2,[1,3]],[1,2,3]]
+    value  = int(input("Enter the value to be searched"))
     print(listItems(itemList,2))
 
 if __name__ == '__main__':
